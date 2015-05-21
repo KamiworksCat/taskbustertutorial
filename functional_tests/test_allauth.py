fec47e2904b04a4543d794c3fe228861ec123a57
Wai Keat <waikeat@kamiworks.com>
2015-05-21 11:35:11 +0800

    gitignore update and new contents for google Auth

--- functional_tests/test_allauth.py
+++ functional_tests/test_allauth.py
@@ -1,39 +1,46 @@
-from selenium.webdriver.firefox import webdriver
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
-
+ 
 from django.core.urlresolvers import reverse
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.utils.translation import activate
-
+ 
+ 
 class TestGoogleLogin(StaticLiveServerTestCase):
 
+    fixtures = ['allauth_fixture']
+ 
     def setUp(self):
-        self.browser = webdriver.WebDriver()
+        self.browser = webdriver.Firefox()
         self.browser.implicitly_wait(3)
         self.browser.wait = WebDriverWait(self.browser, 10)
         activate('en')
-
+ 
     def tearDown(self):
         self.browser.quit()
-
+ 
     def get_element_by_id(self, element_id):
-        return self.browser.wait.until(EC.presence_of_element_located(By.ID, element_id))
-
+        return self.browser.wait.until(EC.presence_of_element_located(
+                (By.ID, element_id)))
+ 
     def get_button_by_id(self, element_id):
-        return self.browser.wait.until(EC.element_to_be_clickable(By.ID, element_id))
-
+        return self.browser.wait.until(EC.element_to_be_clickable(
+                (By.ID, element_id)))
+ 
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)
-
+ 
     def test_google_login(self):
         self.browser.get(self.get_full_url("home"))
         google_login = self.get_element_by_id("google_login")
         with self.assertRaises(TimeoutException):
             self.get_element_by_id("logout")
-        self.assertEqual(google_login.get_attribute("href"), self.live_server_url + "/accounts/google/login")
+        self.assertEqual(
+            google_login.get_attribute("href"),
+            self.live_server_url + "/accounts/google/login")
         google_login.click()
         with self.assertRaises(TimeoutException):
             self.get_element_by_id("google_login")
