8428736f810d25b8c06a479e88618e730ba55f44
Wai Keat <waikeat@kamiworks.com>
2015-05-21 14:09:28 +0800

    Amend and changes made

--- functional_tests/test_allauth.py
+++ functional_tests/test_allauth.py
@@ -32,7 +32,17 @@ class TestGoogleLogin(StaticLiveServerTestCase):
  
     def get_full_url(self, namespace):
         return self.live_server_url + reverse(namespace)
- 
+
+    def user_login(self):
+        import json
+        with open("taskbuster/fixtures/google_user.json") as f:
+            credentials = json.loads(f.read())
+        for key, value in credentials.items():
+            self.get_element_by_id(key).send_keys(value)
+        for btn in ["signIn", "submit_approve_access"]:
+            self.get_button_by_id(btn).click()
+        return
+
     def test_google_login(self):
         self.browser.get(self.get_full_url("home"))
         google_login = self.get_element_by_id("google_login")
@@ -42,6 +52,7 @@ class TestGoogleLogin(StaticLiveServerTestCase):
             google_login.get_attribute("href"),
             self.live_server_url + "/accounts/google/login")
         google_login.click()
+        self.user_login()
         with self.assertRaises(TimeoutException):
             self.get_element_by_id("google_login")
         google_logout = self.get_element_by_id("logout")
